e4fb503137be97cc017aa582c199e211fec6540b
Wayne Yu <3154816042@qq.com>
2019-07-29 17:26:08 +0800

    Update processparse

--- src/ProcessParse.cpp
+++ src/ProcessParse.cpp
@@ -23,7 +23,6 @@ std::string ProcessParse::getVmSize(std::string pid)
 std::string ProcessParse::getCpuPercent(std::string pid)
 {
     std::string line;
-    std::string value;
     float result;
     ifstream stream = Util::getStream((Path::basePath() + pid + Path::statPath()));
     std::getline(stream, line);
@@ -32,26 +31,24 @@ std::string ProcessParse::getCpuPercent(std::string pid)
     std::vector<string> values(beg, end);
 
     // acquiring relevant times for calculation of active occupation of CPU for selected process
-    float utime = stof(ProcessParse::getProcUpTime(pid));
-    float stime = stof(values[14]);
-    float cutime = stof(values[15]);
-    float cstime = stof(values[16]);
+    float utime = stof(ProcessParse::getProcUpTime(pid)); // 该任务在用户态运行的时间，单位为jiffies
+    float stime = stof(values[14]); // 该任务在核心态运行的时间，单位为jiffies
+    float cutime = stof(values[15]); // 所有已死线程在用户态运行的时间，单位为jiffies
+    float cstime = stof(values[16]); // 所有已死在核心态运行的时间，单位为jiffies
     float starttime = stof(values[21]);
     float uptime = ProcessParse::getSysUpTime();
-    float freq = sysconf(_SC_CLK_TCK);
-    float total_time = utime + stime + cutime + cstime;
+    float freq = sysconf(_SC_CLK_TCK); // 时钟滴答的频率（HZ）：也即1秒时间内PIT所产生的时钟滴答次数
+    float total_time = utime + stime + cutime + cstime; // 进程的总Cpu时间，该值包括其所有线程的cpu时间。
     float seconds = uptime - (starttime / freq);
     result = 100.0 * ((total_time / freq) / seconds);
 
     return to_string(result);
 }
 
-
+// 该任务在用户态运行的时间，单位为jiffies
 std::string ProcessParse::getProcUpTime(std::string pid)
 {
     std::string line;
-    std::string value;
-    float result;
     ifstream stream = Util::getStream((Path::basePath() + pid + "/" + Path::statPath()));
     std::getline(stream, line);
     std::istringstream buf(line);
@@ -60,4 +57,22 @@ std::string ProcessParse::getProcUpTime(std::string pid)
 
     // Using sysconf to get clock ticks of the host machine
     return to_string(float(stof(values[13])/sysconf(_SC_CLK_TCK)));
+}
+
+// 系统运行时间
+long int ProcessParse::getSysUpTime()
+{
+    std::string line;
+    ifstream stream = Util::getStream((Path::basePath() + Path::upTimePath()));
+    getline(stream, line);
+    std::istringstream buf(line);
+    std::istring_iterator<string> beg(buf), end;
+    std::vector<std::string> values(beg, end);
+
+    return stoi(values[0]);
+}
+
+std::string getProcUser(std::string pid)
+{
+
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <unistd.h>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <thread>
+#include <chrono>
+#include <iterator>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <cerrno>
+#include <cstring>
+#include <dirent.h>
+#include <time.h>
+#include <unistd.h>
 using std::cout;
 
 int main(int argc, char const *argv[])
 {
-    cout << "sdfsdf: " << "\n";
     // Using sysconf to get clock ticks of the host machine
-    /**
-     * 
-     * 
-     * process up time
-     * clock ticks
-     * freq = sysconf(_SC_CLK_TCK);
-     * istringstream buf(line);
-     * std::istream_iterator<string> beg(buf), end;
-     * std::vector<string> values(beg, end);
-     * 
-     *
-     * 
-     */
     cout << sysconf(_SC_CLK_TCK) << "\n";
+    std::ifstream stream("/home/sun/working_projects/服务器登录");
+    std::string line;
+    while (std::getline(stream, line))
+    {
+        cout << line << "\n";
+        std::istringstream buf(line);
+        std::istream_iterator<std::string> beg(buf), end;
+        // cout << beg << "\n";
+        std::vector<std::string> values(beg, end);
+        for (int i = 0; i < values.size(); i++)
+        {
+            cout << values[i] << "\n";
+        }
+        
+    }
     return 0;
 }
